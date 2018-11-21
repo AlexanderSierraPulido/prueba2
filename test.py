145bd0d8c7805a9374e31a6fa0e654dbb7241d14
AlexanderSierraPulido <a.sierra12@uniandes.edu.co>
2018-11-21 18:24:57 +0000

    juu

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-print("hola mundo cruel")
+print("Voy a hacer lo posible por pasar esta materia, a pesar de que entiendo los temas pero mis notas no son lo mejor.")
